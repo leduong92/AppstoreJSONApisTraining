67035c40c3d3e56fb3c1538f52f290b405ef27ea
Mai Le Duong <maileduong@Mais-Mac.local>
2019-08-01 02:28:24 -0700

    Initial Commit

---
+++ AppJSONApisTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppJSONApisTraining
+//
+//  Created by Mai Le Duong on 8/1/19.
+//  Copyright © 2019 Mai Le Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
