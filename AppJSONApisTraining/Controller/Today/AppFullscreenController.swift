1369d51dce1f0408af5e4945f8fb69273b79d732
Mai Le Duong <maileduong@Mais-Mac.local>
2019-08-02 08:26:26 +0700

    Starting Frame

---
+++ AppJSONApisTraining/Controller/Today/AppFullscreenController.swift
@@ -0,0 +1,28 @@
+//
+//  AppFullscreenController.swift
+//  AppJSONApisTraining
+//
+//  Created by Mai Le Duong on 8/1/19.
+//  Copyright © 2019 Mai Le Duong. All rights reserved.
+//
+
+import UIKit
+
+class AppFullscreenController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return TodayController.todayCellHeight
+    }
+}
--- AppJSONApisTraining/Controller/Today/TodayController.swift
+++ AppJSONApisTraining/Controller/Today/TodayController.swift
@@ -15,11 +15,13 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.isNavigationBarHidden = true
         collectionView.backgroundColor = #colorLiteral(red: 0.9274409003, green: 0.9274409003, blue: 0.9274409003, alpha: 1)
         
         collectionView.register(TodayCell.self, forCellWithReuseIdentifier: todayCellId)
         
     }
+    var appFullscreenController: AppFullscreenController!
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         showSingleAppFullScreen(indexPath: indexPath)
@@ -28,11 +30,40 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     fileprivate func showSingleAppFullScreen(indexPath: IndexPath) {
         // #1
         setupSingleAppFullscreenController(indexPath)
+        
+        //# 2 setup fullscreen in its starting position
+        setupAppFullscreenStartingPosition(indexPath)
     }
+    
     fileprivate func setupSingleAppFullscreenController(_ indexPath: IndexPath) {
-        let appFullscreenController = UIViewController()
-        appFullscreenController.view.backgroundColor = .red
+        print("clicking into today cell:", indexPath)
+        let appFullscreenController = AppFullscreenController()
+        
+        appFullscreenController.view.layer.cornerRadius = 16
     }
+    
+    fileprivate func setupAppFullscreenStartingPosition(_ indexPath: IndexPath) {
+        let fullscreenView = appFullscreenController.view!
+        view.addSubview(fullscreenView)
+        
+        setupStartingCellFrame(indexPath)
+        
+        guard let startingFrame = self.startingFrame else { return }
+        
+        fullscreenView.frame = startingFrame
+    }
+    var startingFrame: CGRect?
+    
+    fileprivate func setupStartingCellFrame(_ indexPath: IndexPath) {
+        
+        guard let cell = collectionView.cellForItem(at: indexPath) else { return }
+        
+        guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else { return }
+        
+        self.startingFrame = startingFrame
+        
+    }
+    
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
